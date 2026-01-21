a9f7457f417f729a748b540be121ed7460e95ee4
Yuki S <yuki.schaefer@googlemail.com>
2026-01-21 13:46:32 +0100

    Minor fix

--- src/views/Dashboard/transcactions/transcactions.vue
+++ src/views/Dashboard/transcactions/transcactions.vue
@@ -50,14 +50,14 @@ const filteredTransaction = computed(() => {
 
 
 function filterState(t: TransactionAuthResponse<ExpandTransaction>, filter: string) {
-  return t.expand?.transaction.title.toLowerCase().includes(filter.toLowerCase()) ||
-    t.expand?.transaction.type.toLowerCase().includes(filter.toLowerCase()) ||
-    t.state.toLowerCase().includes(filter.toLowerCase()) ||
-    (t.expand?.transaction.expand?.milestone?.title?.toLowerCase().includes(filter.toLowerCase()) ?? false) ||
-    t.expand?.transaction.amount.toString().includes(filter.toLowerCase()) ||
-    t.expand?.transaction.amount_bar.toString().includes(filter.toLowerCase()) ||
-    t.expand?.transaction.expand?.createdby.name.toLowerCase().includes(filter.toLowerCase()) ||
-    t.expand?.transaction.expand?.createdby.email.toLowerCase().includes(filter.toLowerCase());
+  const lowerFilter = filter.toLowerCase();
+  return t.expand?.transaction.title.toLowerCase().includes(lowerFilter) ||
+    t.expand?.transaction.type.toLowerCase().includes(lowerFilter) ||
+    t.state.toLowerCase().includes(lowerFilter) ||
+    (t.expand?.transaction.expand?.milestone?.title?.toLowerCase().includes(lowerFilter) ?? false) ||
+    t.expand?.transaction.amount.toString().includes(lowerFilter) ||
+    t.expand?.transaction.amount_bar.toString().includes(lowerFilter) ||
+    t.expand?.transaction.expand?.createdby.name.toLowerCase().includes(lowerFilter);
 }
 </script>
 
